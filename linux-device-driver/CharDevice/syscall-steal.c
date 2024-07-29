f5cf05d155cb6a2c7dca8d905342ae373ef09b05
ducndc <nguyendangchungduc1999@gmail.com>
2024-07-29 10:10:14 +0700

    [DEVICE_DRIVER]: Update syscall-steal.c

--- linux-device-driver/CharDevice/syscall-steal.c
+++ linux-device-driver/CharDevice/syscall-steal.c
@@ -106,4 +106,158 @@ static asmlinkage long (*original_call)(int, const char __user *, int, umode_t);
 #endif
 
 /**
- * This function we wil
+ * This function we will replace sys_openat (the function called when you call
+ * the open system call) with. To find the exact prototype, with the number and type
+ * of arguments, we find the original function first (it is at fs/open.c).
+ *
+ * In theory, this means that we are tied to the current version of the kernel. In
+ * practice, the system calls almost never change (it would wreck havoc and require
+ * programs to be recomplied, since the system calls are the interface between the 
+ * kernel and the processes).
+ */
+#ifdef CONFIG_ARCH_HAS_SYSCALL_WRAPPER
+static asmlinkage long our_sys_openat(const struct pt_regs *regs)
+#else
+static asmlinkage long our_sys_openat(int dfd, const char __user *filename,
+                                      int flags, umode_t mode)
+#endif
+{
+  int i = 0;
+  char ch;
+  if (__kuid_val(current_uid()) != uid)
+    goto orig_call;
+  /* Report the file, if relevant */
+  pr_info("Opened file by %d: ", uid);
+  do {
+#ifdef CONFIG_ARCH_HAS_SYSCALL_WRAPPER
+    get_user(ch, (char __user *)regs->si + i);
+#else
+    get_user(ch, (char __user *)filename + i);
+#endif
+    i++;
+    pr_info("%c", ch);
+  } while (ch != 0);
+  pr_info("\n");
+orig_call:
+  /**
+   * Call the original sys_openat - otherwise, we lose the ability to open files
+   */
+#ifdef CONFIG_ARCH_HAS_SYSCALL_WRAPPER
+  return original_call(regs);
+#else
+  return original_call(dfd, filename, flags, mode);
+#endif
+}
+
+static unsigned long **acquire_sys_call_table(void)
+{
+#ifdef HAVE_KSYS_CLOSE
+  unsigned long int offset = PAGE_OFFSET;
+  unsigned long **sct;
+  while (offset < ULLONG_MAX) {
+    sct = (unsigned long **)offset;
+    if (sct[__NR_CLOSE] == (unsigned long *)ksys_close)
+      return sct;
+    offset += sizeof(void *);
+  }
+  return NULL;
+#endif 
+#ifdef HAVE_PARAM
+  const char sct_name[15] = "sys_call_table";
+  char symbol[40] = {0};
+  if (sym == 0) {
+    pr_alert("For Linux v5.7+, Kprobes is the preferable way to get symbol.\n");
+    pr_info("If Kprobes is absent, you have to specify the address of sys_call_table symbol\n");
+    pr_info("by /boot/System.map or /proc/kallsyms, which contains all the symbol addresses, into sym parameter.\n");
+    return NULL;
+  }
+  sprint_symbol(symbol, sym);
+  if (!strncmp(sct_name, symbol, sizeof(sct_name) - 1))
+    return (unsigned long **)sym;
+  return NULL;
+#endif
+#ifdef HAVE_KPROBES
+  unsigned long (*kallsyms_lookup_name)(const char *name);
+  struct kprobe kp = {
+    .symbol_name = "kallsyms_lookup_name",
+  };
+  if (register_kprobe(&kp) < 0)
+    return NULL;
+  kallsyms_lookup_name = (unsigned long (*)(const char *name))kp.addr;
+  unregister_kprobe(&kp);
+#endif
+  return (unsigned long **)kallsyms_lookup_name("sys_call_table");
+}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 3, 0)
+static inline void __write_cr0(unsigned long cr0)
+{
+  asm volatile("mov %0,%%cr0" : "+r"(cr0) : : "memory");
+}
+#else
+#define __write_cr0 write_cr0
+#endif
+
+static void enable_write_protection(void)
+{
+  unsigned long cr0 = read_cr0();
+  set_bit(16, &cr0);
+  __write_cr0(cr0);
+}
+#endif
+
+static int __init syscall_steal_start(void)
+{
+#if USE_KPROBES_PRE_HANDLER_BEFORE_SYSCALL
+  int err;
+  /* use symbol name from the module parameter */
+  syscall_kprobe.symbol_name = syscall_sym;
+  err = register_kprobe(&syscall_kprobe);
+  if (err) {
+    pr_err("register_kprobe() on %s failed: %d\n", syscall_sym, err);
+    pr_err("Please check the symbol name from 'syscall_sym' parameter.\n");
+    return err;
+  }
+#else
+  if (!(sys_call_table_stolen = acquire_sys_call_table()))
+    return -1;
+  disable_write_protection();
+  /* keep track of the original open function */
+  original_call = (void *)sys_call_table_stolen[__NR_openat];
+  /* use our openat function instead */
+  sys_call_table_stolen[__NR_openat] = (unsigned long *)our_sys_openat;
+  enable_write_protection();
+  #endif
+
+  pr_info("Spying on UID:%d\n", uid);
+  return 0;
+}
+
+static void __exit syscall_steal_end(void)
+{
+#if USE_KPROBES_PRE_HANDLER_BEFORE_SYSCALL
+  unregister_kprobe(&syscall_kprobe);
+#else
+  if (!sys_call_table_stolen)
+      return;
+
+  /* Return the system call back to normal */
+  if (sys_call_table_stolen[__NR_openat] != (unsigned long *)our_sys_openat) {
+      pr_alert("Somebody else also played with the ");
+      pr_alert("open system call\n");
+      pr_alert("The system may be left in ");
+      pr_alert("an unstable state.\n");
+  }
+
+  disable_write_protection();
+  sys_call_table_stolen[__NR_openat] = (unsigned long *)original_call;
+  enable_write_protection();
+#endif
+
+  msleep(2000);
+}
+
+module_init(syscall_steal_start);
+module_exit(syscall_steal_end);
+
+MODULE_LICENSE("GPL");
